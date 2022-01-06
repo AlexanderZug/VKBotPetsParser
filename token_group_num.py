5b1d5f140e7f7405ef5740601aa41fc271bf5586
AlexanderZug <alexandertsygankov.unterwegs@.gmail.com>
2022-01-06 14:50:32 +0300

    Token's renaming

--- token_group_num.py
+++ token_group_num.py
@@ -1,4 +1,6 @@
 import os
 
-TOKEN = os.environ.get('TOKEN')
-VK_GROUP_NUM = os.environ.get('GROUP_ID')
+TOKEN = '69af5b241e8bd98f214ea1ff53d4a141e36b413f2f20239c5428dbda305b1fc6f19439d7ecfcc82a51773'
+VK_GROUP_NUM = 209054655
+# TOKEN = os.environ.get('TOKEN')
+# VK_GROUP_NUM = os.environ.get('GROUP_ID')
